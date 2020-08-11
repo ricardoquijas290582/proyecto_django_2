3624e73e19d171d4a81760f88334ea296c496d07
Ricardo Quijas <ricardo@gmail.com>
2020-08-10 22:55:25 -0500

    Nueva actualización

--- first_project/movies/views.py
+++ first_project/movies/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django import views
+from .models import Movie
 
 # Create your views here.
+
+def GetMovies(request):
+    # Vamos a traer todos las películas
+    movies = Movie.objects.all() # SELECT * FROM movies_movie #queryset
+    print(movies)
+    return HttpResponse('Sí funciona mi estimado, vamos bien!')
+    #template_name = 'movies/list.html'
+    #context = {
+    #    'movies': movies
+    #}
+    #return render(request, template_name, context)
