7f1a3c27f6b70352ef31afa6e99a84a5bfa97620
Ricardo Quijas <ricardo@gmail.com>
2020-08-17 19:45:59 -0500

    Nueva actualización

--- first_project/movies/views.py
+++ first_project/movies/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django import views
 from .models import Movie
+from .forms import MovieForm
 
 # Create your views here.
+# Hay dos formas de crear vistas
+# 1.- Creando una clase -> Tenemos dos o más métodos HTTP en la misma ruta
+# 2.- Creando una función -> Sólo tenemos un método HTTP en la ruta
+
 
 def GetMovies(request):
     # Vamos a traer todos las películas
@@ -24,3 +29,46 @@ def GetMovie(request, id):
         'movie': movie
     }
     return render(request, template_name, context)
+
+class CreateMovie(views.View):
+    def get(self, request):
+        template_name = 'movies/form.html'
+        return render(request, template_name)
+
+    def post(self, request):
+        data = request.POST
+        new_movie = Movie.objects.create(
+            title=data['title'],
+            sinopsis=data['sinopsis'],
+            duration=data['duration'],
+            calif=data['calif'],
+            gender=data['gender']
+        )
+        if new_movie:
+            print('Película creada con éxito!')
+            return redirect('/movies/')
+        else:
+            print('La película no se pudo crear')
+            return redirect('/movies/create/')
+
+class CreateMovieEasy(views.View):
+    def get(self, request):
+        form = MovieForm()
+        template_name = 'movies/form_easy.html'
+        context = {
+            'form': form
+        }
+        return render(request, template_name, context)
+
+    def post(self, request):
+        new_form = MovieForm(request.POST)
+        if new_form.is_valid():
+            new_movie = new_form.save()
+            print('Se creó la película corerctamente', new_movie)
+            return redirect('/movies/')
+        else:
+            template_name = 'movies/form_easy.html'
+            context = {
+                'form': new_form
+            }
+            return render(request, template_name, context)
