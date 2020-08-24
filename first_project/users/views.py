ffce3aeedff717d24bb50e7125dbc91ee8b91c38
Ricardo Quijas <ricardo@gmail.com>
2020-08-24 17:19:06 -0500

    Nueva actualización

---
+++ first_project/users/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+
+# Create your views here.
+def GetUsers(request):
+    users = User.objects.filter(is_superuser=False)
+    template_name = "users/list.html"
+    context = {
+        'users': users
+    }
+    return render(request, template_name, context)
